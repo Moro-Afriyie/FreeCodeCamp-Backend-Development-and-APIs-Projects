7d6a7bab12a34da0429d57fcc5bb69aa18ee3649
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-26 11:58:08 +0000

    done with  the api

---
+++ Timestamp-Microservice/boilerplate-project-timestamp/server.js
@@ -0,0 +1,53 @@
+// server.js
+// where your node app starts
+
+// init project
+const express = require("express");
+const app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+const cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+
+app.get("/api/", (req, res) => {
+  const date = new Date();
+  res.json({ unix: date.getTime(), utc: date.toUTCString() });
+});
+
+app.get("/api/:date", (req, res) => {
+  app.get("/api/:date", (req, res) => {
+    let dateValue = req.params.date;
+    if (/^-?\d+$/.test(dateValue)) {
+      dateValue = parseInt(dateValue);
+    }
+
+    const date = new Date(dateValue);
+    if (date.getTime()) {
+      res.json({ unix: date.getTime(), utc: date.toUTCString() });
+    } else {
+      res.json({ error: "Invalid Date" });
+    }
+  });
+});
+
+// listen for requests :)
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+const listener = app.listen(8000, function () {
+  console.log("Your app is listening on port " + 8000);
+});

0e7c9e816c7e3e09c09fdab84fd156bca3d5ee53
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-26 17:03:12 +0000

    set up my mogodb database

--- URL Shortener Microservice/server.js
+++ URL Shortener Microservice/server.js
@@ -1,17 +1,22 @@
+const mydbUri = process.env["DB_URI"];
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const dns = require("dns");
 const mongoose = require("mongoose");
+const urlParser = require("url");
 const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
+mongoose
+  .connect(mydbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("connected to mongoDB"))
+  .catch((err) => console.log(err));
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(urlencoded({ extended: true }));
 
 app.use("/public", express.static(`${process.cwd()}/public`));
 
@@ -22,11 +27,6 @@ app.get("/", function (req, res) {
 // Your first API endpoint
 app.get("/api/hello", function (req, res) {
   res.json({ greeting: "hello API" });
-});
-
-app.post("/api/shorturl", (req, res) => {
-  console.log(req.body);
-  // console.log(dns.lookup(req.body));
 });
 
 app.listen(port, function () {

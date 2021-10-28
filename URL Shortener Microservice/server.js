cbc52369cbe4e9089a36c8bfa84eedf6e1a85f73
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-28 08:28:27 +0000

    removed the body-parser module

--- URL Shortener Microservice/server.js
+++ URL Shortener Microservice/server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const cors = require("cors");
 const dns = require("dns");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const urlParser = require("url");
 const app = express();
 
@@ -17,7 +16,7 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 // create a schema
 const UrlSchema = mongoose.Schema({

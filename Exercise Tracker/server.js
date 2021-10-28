08ba0170ab86b6617efe235017a1f3e02168f868
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-28 13:24:38 +0000

    create a UserModel

--- Exercise Tracker/server.js
+++ Exercise Tracker/server.js
@@ -8,6 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });

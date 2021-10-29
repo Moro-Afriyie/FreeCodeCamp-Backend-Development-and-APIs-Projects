d85bad04d58b99531b8507b709b18c42fe6dafde
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-29 20:31:54 +0000

    add the get users route

--- Exercise Tracker/routes/users.js
+++ Exercise Tracker/routes/users.js
@@ -3,6 +3,15 @@ const router = express.Router();
 const User = require("../models/userModel");
 const Excercise = require("../models/exercise");
 
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 router.post("/", async (req, res) => {
   const username = req.body.username;
   if (!username) {

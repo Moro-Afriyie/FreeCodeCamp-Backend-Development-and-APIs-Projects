f4bf02a56c3d7f3af563db95da902663dac49d0f
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-29 19:58:47 +0000

    add the routing to the user.js

--- Exercise Tracker/routes/users.js
+++ Exercise Tracker/routes/users.js
@@ -1,2 +1,47 @@
 const express = require("express");
 const router = express.Router();
+
+router.post("/api/users", async (req, res) => {
+  const username = req.body.username;
+  if (!username) {
+    return res.status(404).json({ error: "invalid username" });
+  }
+  User.create(
+    {
+      username: username,
+    },
+    (err, data) => {
+      res.json({ username: data.username, _id: data.id });
+    }
+  );
+});
+
+router.post("/api/users/:_id/exercises", async (req, res) => {
+  const userName = await User.findById(req.params._id);
+  console.log(userName.username);
+  // if (!id) {
+  //   return res.status(404).json({ error: "invalid id" });
+  // }
+  if (!req.body) {
+    return res.status(404).json({ error: "invalid details" });
+  }
+  Excercise.create(
+    {
+      userid: req.params._id,
+      username: userName.username,
+      description: req.body.description,
+      duration: req.body.duration,
+      date: req.body.date || new Date(),
+    },
+    (err, data) => {
+      // res.json({
+      //   username: data.username,
+      //   description: data.description,
+      //   duration: data.duration,
+      //   date: data.date.toDateString(),
+      //   _id: data.userid,
+      // });
+      console.log(data);
+    }
+  );
+});
--- Exercise Tracker/server.js
+++ Exercise Tracker/server.js
@@ -21,50 +21,6 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.post("/api/users", async (req, res) => {
-  const username = req.body.username;
-  if (!username) {
-    return res.status(404).json({ error: "invalid username" });
-  }
-  User.create(
-    {
-      username: username,
-    },
-    (err, data) => {
-      res.json({ username: data.username, _id: data.id });
-    }
-  );
-});
-
-app.post("/api/users/:_id/exercises", async (req, res) => {
-  const userName = await User.findById(req.params._id);
-  console.log(userName.username);
-  // if (!id) {
-  //   return res.status(404).json({ error: "invalid id" });
-  // }
-  if (!req.body) {
-    return res.status(404).json({ error: "invalid details" });
-  }
-  Excercise.create(
-    {
-      userid: req.params._id,
-      username: userName.username,
-      description: req.body.description,
-      duration: req.body.duration,
-      date: req.body.date || new Date(),
-    },
-    (err, data) => {
-      // res.json({
-      //   username: data.username,
-      //   description: data.description,
-      //   duration: data.duration,
-      //   date: data.date.toDateString(),
-      //   _id: data.userid,
-      // });
-      console.log(data);
-    }
-  );
-});
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
 });

2cc002b11adfa40cd75ef9e3eb68c1803e1acb61
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-31 18:00:00 -0100

    delete the logs and exercise model

--- Exercise Tracker/routes/users.js
+++ Exercise Tracker/routes/users.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
-const Excercise = require("../models/exercise");
-const Log = require("../models/log");
 
 router.get("/", async (req, res) => {
   try {

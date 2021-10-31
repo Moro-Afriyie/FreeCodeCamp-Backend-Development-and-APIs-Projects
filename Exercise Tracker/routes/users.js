2f320b5488909dbc718dc0f456a21f06b0343e32
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-31 12:57:10 -0100

    modify the findByIdAndUpdate method

--- Exercise Tracker/routes/users.js
+++ Exercise Tracker/routes/users.js
@@ -36,7 +36,7 @@ router.post("/:_id/exercises", async (req, res) => {
   const description = req.body.description;
   const duration = req.body.duration;
   const date = req.body.date || new Date();
-  const user = await User.findByIdAndUpdate({ _id: id }, {}, { new: true });
+  const user = await User.findByIdAndUpdate(id, {}, { new: true });
   console.log(user);
   // if (!id) {
   //   return res.status(404).json({ error: "invalid id" });

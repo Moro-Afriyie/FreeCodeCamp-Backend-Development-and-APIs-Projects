32012fb93bc39c146a918fc6ca147863bc66a99d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-31 18:19:29 -0100

    allow only usernames to show

--- Exercise Tracker/routes/usersRoute.js
+++ Exercise Tracker/routes/usersRoute.js
@@ -4,7 +4,7 @@ const User = require("../models/userModel");
 
 router.get("/", async (req, res) => {
   try {
-    const users = await User.find();
+    const users = await User.find({}, { username: 1 });
     res.send(users);
   } catch (error) {
     console.log(error);

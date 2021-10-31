4ec4c398b93b8e3034d00c35a10d47b401ab2504
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-10-31 12:29:20 -0100

    refactor the models

--- Exercise Tracker/models/userModel.js
+++ Exercise Tracker/models/userModel.js
@@ -2,6 +2,17 @@ const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
   username: { type: String, required: true },
+  count: { type: Number, required: true },
+  log: [
+    {
+      description: String,
+      duration: Number,
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
 });
 
 const UserModel = mongoose.model("User", UserSchema);

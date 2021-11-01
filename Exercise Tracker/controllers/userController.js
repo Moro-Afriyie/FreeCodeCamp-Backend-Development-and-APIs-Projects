2a613722e4d9e7139a09f6e437b2690243d1849d
Moro Owusu Afriyie <ceremoinc99@gmail.com>
2021-11-01 20:33:05 -0100

    create getUser function

---
+++ Exercise Tracker/controllers/userController.js
@@ -0,0 +1,12 @@
+const User = require("../models/userModel");
+
+const getUser = async (req, res) => {
+  try {
+    const users = await User.find({}, { username: 1 });
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = userController;
